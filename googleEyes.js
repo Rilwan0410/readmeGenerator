fb65c2eb246449e5f67bc01cbe7090d7b8bb19f3
User <user@Users-MacBook-Air.local>
2023-11-28 20:37:23 -0500

    added dependencies and added functionality to writeToFile() function

---
+++ googleEyes.js
@@ -0,0 +1,5 @@
+const assCrack = "water";
+
+function whereSweater() {
+  console.log("I am wearing a sweater");
+}
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.log(err) : console.log("successfully added file");
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
